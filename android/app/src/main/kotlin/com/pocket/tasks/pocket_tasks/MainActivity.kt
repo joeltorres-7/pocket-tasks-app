3335310aab4551868502976754fc655a4a834982
Joel Torres <thereaderboy.7@outlook.com>
2023-08-17 11:47:52 -0500

    develop - Implements splash screen.

--- android/app/src/main/kotlin/com/pocket/tasks/pocket_tasks/MainActivity.kt
+++ android/app/src/main/kotlin/com/pocket/tasks/pocket_tasks/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.pocket.tasks.pocket_tasks
 
+import SplashView
+import android.window.SplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): io.flutter.embedding.android.SplashScreen? = SplashView()
 }
