5d2e6dada33f309a55a1caa43b5ec4b1b6f26fc3
Joel Torres <thereaderboy.7@outlook.com>
2024-01-11 10:53:51 -0500

    MVP-010 - Implements splash effect on task cards.

--- lib/views/components/task_card.dart
+++ lib/views/components/task_card.dart
@@ -32,95 +32,94 @@ class _TaskCardState extends State<TaskCard> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return GestureDetector(
-      onTap: widget.onCardTap,
-      child: AnimatedOpacity(
-        duration: const Duration(milliseconds: 100),
-        opacity: isChecked ? 0.5 : 1.0,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            border: Border.all(color: Theme.of(context).colorScheme.outlineVariant),
-            borderRadius: BorderRadius.circular(16.0),
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).colorScheme.shadow,
-                spreadRadius: 1,
-                blurRadius: 6,
-                offset: const Offset(0, 1),
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: screenWidth * 0.6,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              widget.title,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                          ),
-                          VerticalSpacing(4.0),
-                          widget.hasDescription
-                              ? Text(
-                              widget.description,
-                              style: Theme.of(context).textTheme.bodySmall,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                          )
-                              : Text(
-                              AppLocalizations.of(context)!.noDescription,
-                              style: Theme.of(context).textTheme.labelSmall,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
+    return Material(
+      borderRadius: BorderRadius.circular(16.0),
+      color: Theme.of(context).colorScheme.surface,
+      shadowColor: Theme.of(context).colorScheme.shadow,
+      elevation: 8,
+      child: InkWell(
+        onTap: widget.onCardTap,
+        borderRadius: BorderRadius.circular(16.0),
+        splashColor: Theme.of(context).colorScheme.onSurfaceVariant,
+        child: AnimatedOpacity(
+          duration: const Duration(milliseconds: 100),
+          opacity: isChecked ? 0.5 : 1.0,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colorScheme.outlineVariant),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: screenWidth * 0.6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                widget.title,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                            ),
+                            VerticalSpacing(4.0),
+                            widget.hasDescription
+                                ? Text(
+                                widget.description,
+                                style: Theme.of(context).textTheme.bodySmall,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                            )
+                                : Text(
+                                AppLocalizations.of(context)!.noDescription,
+                                style: Theme.of(context).textTheme.labelSmall,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Checkbox(
-                      checkColor: AppColors.primaryWhite,
-                      fillColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
-                      value: isChecked,
-                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
-                      onChanged: (bool? value) {
-                        widget.onChecked();
-                        setState(() {
-                          if (!isChecked) {
-                            AudioManager.playFromName('completed.wav');
-                          }
-                          isChecked = value!;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                VerticalSpacing(12.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    PriorityChip(priorityLevel: widget.priority),
-                    IconButton(
-                        onPressed: null, 
-                        icon: Opacity(
-                          opacity: 0.6,
-                          child: Icon(
-                          Icons.arrow_forward_rounded, size: 18.0, color: Theme.of(context).colorScheme.inversePrimary),
-                        )
-                    )
-                  ],
-                ),
-              ],
+                      Checkbox(
+                        checkColor: AppColors.primaryWhite,
+                        fillColor: MaterialStateProperty.all(Theme.of(context).colorScheme.onSecondary),
+                        value: isChecked,
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+                        onChanged: (bool? value) {
+                          widget.onChecked();
+                          setState(() {
+                            if (!isChecked) {
+                              AudioManager.playFromName('completed.wav');
+                            }
+                            isChecked = value!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  VerticalSpacing(12.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PriorityChip(priorityLevel: widget.priority),
+                      IconButton(
+                          onPressed: null,
+                          icon: Opacity(
+                            opacity: 0.6,
+                            child: Icon(
+                            Icons.arrow_forward_rounded, size: 18.0, color: Theme.of(context).colorScheme.inversePrimary),
+                          )
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
